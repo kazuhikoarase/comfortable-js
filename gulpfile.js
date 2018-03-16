556f27f945b3514fda44081cb3f116e269c3a2d8
kazuhiko.arase <kazuhiko.arase@gmail.com>
2018-03-16 21:37:42 +0900

    commit

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const gulp = require('gulp');
 const del = require('del');
 const runSequence = require('run-sequence');
 const rename = require('gulp-rename');
+const order = require('gulp-order');
 const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const jasmine = require('gulp-jasmine');
@@ -27,18 +28,21 @@ gulp.task('watch', function(){
 
 gulp.task('concat-main-css', function() {
   return gulp.src([ 'src/main/js/**/*.css' ])
+    .pipe(order([ '**/*.css']) )
     .pipe(concat(targetName + '.css') )
     .pipe(gulp.dest('lib/') );
 });
 
 gulp.task('concat-main', ['concat-main-css'], function() {
   return gulp.src([ 'src/main/js/**/*.js' ])
+    .pipe(order([ '**/*.js']) )
     .pipe(concat(targetName + '.js') )
     .pipe(gulp.dest('lib/') );
 });
 
 gulp.task('concat-test', function() {
   return gulp.src([ 'src/test/js/**/*.spec.js' ])
+    .pipe(order([ '**/*.js']) )
     .pipe(concat(targetName + '.spec.js') )
     .pipe(gulp.dest('lib/') );
 });
