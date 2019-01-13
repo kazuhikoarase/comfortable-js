82fa7a5a8964473a7743542b76ca9a86e1ca6f69
kazuhiko.arase <kazuhiko.arase@gmail.com>
2019-01-13 20:23:58 +0900

    commit

--- src/test/js/spec-util.js
+++ src/test/js/spec-util.js
@@ -1,23 +1,23 @@
 
 window.SpecUtil = {
 
-    //https://stackoverflow.com/questions/26596123/internet-explorer-9-10-11-event-constructor-doesnt-work
-    /*
-    (function () {
-      if ( typeof window.CustomEvent === "function" ) return false; //If not IE
-    
-      function CustomEvent ( event, params ) {
-        params = params || { bubbles: false, cancelable: false, detail: undefined };
-        var evt = document.createEvent( 'CustomEvent' );
-        evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
-        return evt;
-       }
-    
-      CustomEvent.prototype = window.Event.prototype;
-    
-      window.CustomEvent = CustomEvent;
-    })();
-    */
+  //https://stackoverflow.com/questions/26596123/internet-explorer-9-10-11-event-constructor-doesnt-work
+  /*
+  (function () {
+    if ( typeof window.CustomEvent === "function" ) return false; //If not IE
+  
+    function CustomEvent ( event, params ) {
+      params = params || { bubbles: false, cancelable: false, detail: undefined };
+      var evt = document.createEvent( 'CustomEvent' );
+      evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+      return evt;
+     }
+  
+    CustomEvent.prototype = window.Event.prototype;
+  
+    window.CustomEvent = CustomEvent;
+  })();
+  */ 
   /**
    * 
    */
