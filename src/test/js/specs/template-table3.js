805d9bc336df06af04ae9ce910d59b2d8b3fc83f
kazuhiko.arase <kazuhiko.arase@gmail.com>
2019-01-14 11:39:48 +0900

    commit

--- src/test/js/specs/template-table3.js
+++ src/test/js/specs/template-table3.js
@@ -51,7 +51,7 @@ describe('template-table3', function() {
     document.body.innerHTML = '';
   });
 
-  it('test', function(done) {
+  it('move column, resize column', function(done) {
 
     var table = createTemplateTable();
 
@@ -66,6 +66,7 @@ describe('template-table3', function() {
 
       var $heads = $('.ctj-header');
       expect($heads.length).toBe(8);
+
       var target = $heads[1];
       var off = $(target).offset();
       var w = $(target).outerWidth();
@@ -146,6 +147,98 @@ describe('template-table3', function() {
 
     });
 
+  });
+
+  it('move column by dialog', function(done) {
+
+    var table = createTemplateTable();
+
+    // test
+
+    expect(table.getLockLeft() ).toBe(0);
+    expect(table.getLockTop() ).toBe(1);
+
+    SpecUtil.nextTick(200, function() {
+
+      var $heads = $('.ctj-header');
+      expect($heads.length).toBe(8);
+
+      if ($heads.length == 8) {
+        var target = $heads[2];
+        var off = $(target).offset();
+        SpecUtil.triggerContextMenu(target, function(event) {
+          event.pageX = off.left + 8;
+          event.pageY = off.top + 8;
+        });
+      }
+
+    }).nextTick(200, function() {
+
+      var $menuitem = $('.ctj-menuitem');
+      expect($menuitem.length).toBe(2);
+      if ($menuitem.length == 2) {
+         SpecUtil.triggerMouseEvent($menuitem[1], 'mousedown');
+      }
+ 
+    }).nextTick(50, function() {
+
+      SpecUtil.triggerMouseEvent(document, 'mouseup');
+
+    }).nextTick(100, function() {
+
+      expect($('.ctj-dialog').length).toBe(1);
+      var $cols = $('.ctj-dialog .ctj-listitem');
+      expect($cols.length).toBe(50 + 1);
+
+      var target = $cols[1];
+      var off = $(target).offset();
+      var w = $(target).outerWidth();
+      var h = $(target).outerHeight();
+      var ch = -1.25;
+
+      expect(document.elementFromPoint(off.left, off.top) ).toBe(target);
+
+      SpecUtil.triggerMouseEvent(target, 'mousedown', function(event) {
+        event.pageX = off.left + w / 2;
+        event.pageY = off.top + h / 2;
+      });
+
+      var toCoord = { left: off.left + w / 2, top: off.top + h / 2 + h * ch };
+      var toTarget = document.elementFromPoint(toCoord.left, toCoord.top);
+
+      this.mousemove = function() {
+        SpecUtil.triggerMouseEvent(toTarget, 'mousemove', function(event) {
+          event.pageX = toCoord.left;
+          event.pageY = toCoord.top;
+        });
+      };
+
+      this.mouseup = function() {
+        SpecUtil.triggerMouseEvent(toTarget, 'mouseup', function(event) {
+          event.pageX = toCoord.left;
+          event.pageY = toCoord.top;
+        });
+      };
+
+    }).nextTick(100, function() {
+
+      this.mousemove();
+
+    }).nextTick(100, function() {
+
+      this.mouseup();
+
+    }).nextTick(100, function() {
+
+      var $btn = $('.ctj-dialog .ctj-button');
+      expect($btn.length).toBe(3);
+
+      // click apply
+      SpecUtil.triggerMouseEvent($btn[1], 'click');
+
+    }).nextTick(1000, function() {
+      done();
+    });
   });
 
 });
